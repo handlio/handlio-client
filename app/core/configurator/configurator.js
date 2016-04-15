b9145e34ca2d2b5a83a26053dc8e11201c53a928
VanDalkvist <i_van@list.ru>
2016-04-15 19:57:59 +0300

    preparing for normal angular project structure - moved configurator to directive;

---
+++ app/core/configurator/configurator.js
@@ -0,0 +1,15 @@
+(function (angular) {
+    'use strict';
+
+    var module = angular.module('handlio.client.configurator');
+
+    _directive.$inject = [];
+    module.directive('configurator', _directive);
+
+    function _directive() {
+        return {
+            templateUrl: 'core/configurator/configurator.html'
+        };
+    }
+
+})(angular);
