9e8d661d0d6aa63bb1b1e6ed052da234c74daa2b
VanDalkvist <i_van@list.ru>
2016-04-17 00:02:33 +0300

    cleanup;

--- app/core/hosts/host.controller.js
+++ app/core/hosts/host.controller.js
@@ -14,14 +14,20 @@
         vm.hosts = HostStore.get('list') || [];
 
         vm.new = angular.copy(defaults.host);
-        vm.model = { keys: '' };
+        vm.model = {
+            keys: '',
+            selected: HostStore.get('selected') || vm.hosts[0] || null
+        };
 
-        vm.model.selected = HostStore.get('selected') || vm.hosts[0] || null;
-        if (!!vm.model.selected) {
-            vm.model.selectedUrl = vm.model.selected.url;
-        }
+        (!!vm.model.selected) && (vm.model.selectedUrl = vm.model.selected.url);
 
-        vm.addHost = function (host) {
+        vm.addHost = _addHost;
+        vm.saveSelected = _saveSelected;
+        vm.removeHost = _removeHost;
+
+        // private functions
+
+        function _addHost(host) {
             var newHost = {
                 url: host.url, name: host.name,
                 createdAt: _toTicks(new Date())
@@ -36,18 +42,18 @@
                 createdAt: newHost.createdAt
             });
             $log.info("Added new host - ", newHost.url + ", " + newHost.name);
-        };
+        }
 
-        vm.saveSelected = function (host) {
+        function _saveSelected(host) {
             HostStore.set('selected', host);
             $log.info("Switched to another host - ", host.url + ", " + host.name);
-        };
+        }
 
-        vm.removeHost = function (host, index) {
+        function _removeHost(host, index) {
             vm.hosts.splice(index, 1);
             HostStore.set('list', vm.hosts);
             $log.info("Host was removed - ", host.url + ", " + host.name);
-        };
+        }
 
         // todo: sorting does not work
         /*
