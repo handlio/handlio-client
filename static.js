7ba037d322efd0910b06bdc61eb18f8532128598
VanDalkvist <i_van@list.ru>
2016-04-27 17:37:34 +0300

    added middleware to static server for support html5 mode;

--- static.js
+++ static.js
@@ -1,4 +1,6 @@
 var connect = require('connect');
+var path = require('path');
+var fs = require('fs');
 var serveStatic = require('serve-static');
 var directory = './app';
 
@@ -6,6 +8,19 @@ var app = connect();
 
 var port = 3001;
 app.use(serveStatic(directory));
+app.use(function (req, res) {
+    var indexPath = path.join(directory, 'index.html');
+    var stat = fs.statSync(indexPath);
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fs.createReadStream(indexPath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(res);
+});
 app.listen(port);
 
 console.log('Listening on port ' + port);
