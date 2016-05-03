20b3b54074a238376af3a505f706b47e756514ac
VanDalkvist <i_van@list.ru>
2016-05-03 22:18:47 +0300

    changed livereload task for local network (closes #19);

---
+++ build/livereload.task.js
@@ -0,0 +1,38 @@
+var path = require('path');
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+var paths = {
+    app: path.join(__dirname, '../app')
+};
+
+var config = {
+    html: path.join(paths.app, '/**/*.html'),
+    js: path.join(paths.app, '/**/*.js')
+};
+
+gulp.task('connect', function () {
+    connect.server({
+        root: paths.app,
+        livereload: true,
+        fallback: paths.app + '/index.html',
+        port: 3111
+    });
+});
+
+gulp.task('html', function () {
+    gulp.src(config.html)
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+    gulp.src(config.js)
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch([config.html, config.js], ['html', 'js']);
+});
+
+gulp.task('livereload', ['connect', 'watch']);
