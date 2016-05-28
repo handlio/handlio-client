835f0a66af8415e99315f5813393628d8108dc52
VanDalkvist <i_van@list.ru>
2016-05-28 19:44:02 +0300

    angular module dependency specs;

--- tests/unit/core.spec.js
+++
@@ -1,9 +0,0 @@
-var tape = require('tape');
-
-var test = tape.angular.wrap();
-
-test('Core module preparations', function (assert) {
-    angular.mock.module('handlio.client');
-
-    assert.end();
-});
