6ad5ad2898f30e6f46544504d03ab5760d8dde4d
VanDalkvist <i_van@list.ru>
2016-06-05 22:03:01 +0300

    added tests for 'hosts' controller; (#20)

--- tests/unit/modules/hosts.spec.js
+++ tests/unit/modules/hosts.spec.js
@@ -8,7 +8,7 @@ test("'hosts' directive ", function (assert) {
     angular.mock.module('handlio.client.hosts');
     angular.mock.module('handlio.client.templateCache');
 
-    inject(['$compile', '$rootScope', function ($compile, $rootScope) {
+    inject(['$compile', '$rootScope', '$controller', function ($compile, $rootScope, $controller) {
 
         assert.doesNotThrow(function () {
             var compiled = _compileElement('<div hosts></div>', $rootScope.$new());
@@ -16,6 +16,18 @@ test("'hosts' directive ", function (assert) {
             assert.true(compiled.hasClass('ng-scope'), "has ng-scope class");
             assert.equal(compiled.find('form').length, 1, "has hosts form inside");
         }, "should compile 'hosts' directive successfully");
+
+        var scope = $rootScope.$new();
+        var ctrl = $controller('HostsController', {
+            $scope : scope
+        });
+
+        assert.ok(ctrl.addHost, "method 'addHost' exist in controller");
+        assert.ok(ctrl.list, "object 'list' exist in controller");
+        assert.ok(ctrl.model, "object 'model' exist in controller");
+        assert.ok(ctrl.new, "object 'new' exist in controller");
+        assert.ok(ctrl.removeHost, "method 'removeHost' exist in controller");
+        assert.ok(ctrl.saveSelected, "method 'saveSelected' exist in controller");
 
         function _compileElement(html, scope) {
             var element = angular.element(html);
